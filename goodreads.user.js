7844ffd933bb34ffdda402245c4ba0d62fdb07cf
Hezekiah Michael <pianycist@gmail.com>
2022-07-16 04:29:50 -0400

    add clipboard functionality

--- goodreads.user.js
+++ goodreads.user.js
@@ -8,9 +8,10 @@
 // @author      holyspiritomb
 // @run-at      document-end
 // @icon        https://www.google.com/s2/favicons?domain=goodreads.com
-// @description Adds mobi-asin to a more convenient place.
+// @description Adds mobi-asin to individual book pages and copies it to the clipboard when clicked. On desktop, clicking "mobi-asin:" will copy the asin to the clipboard with "mobi-asin:" as a prefix for easy pasting into Calibre ebook library software. Works on book pages that have a button to buy from the Kindle Store or are listed under the book details as a Kindle edition.
 // ==/UserScript==
 
+// inspired by CH Amazon ASIN Adder by clickhappier https://greasyfork.org/en/scripts/6862-ch-amazon-asin-adder
 
 
 
@@ -27,3 +28,9 @@ $('td.field.title').each(function(){
     $(this).append(bookAuthorElem);
     $(this).append(asinDiv);
 });
+
+$('div.injectedAsin').on('click',function(){
+        asin = $(this).children('div.value')[0].innerText;
+        GM_setClipboard(asin);
+        $(this).append("\u2713");
+    });
