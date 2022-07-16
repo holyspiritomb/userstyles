53b3290d9b4c5a15c2551583e0f7a1ffe886b021
Hezekiah Michael <pianycist@gmail.com>
2022-07-16 04:32:09 -0400

    updated

--- goodreads.user.js
+++ goodreads.user.js
@@ -8,14 +8,11 @@
 // @author      holyspiritomb
 // @run-at      document-end
 // @icon        https://www.google.com/s2/favicons?domain=goodreads.com
-// @description Adds mobi-asin to individual book pages and copies it to the clipboard when clicked. On desktop, clicking "mobi-asin:" will copy the asin to the clipboard with "mobi-asin:" as a prefix for easy pasting into Calibre ebook library software. Works on book pages that have a button to buy from the Kindle Store or are listed under the book details as a Kindle edition.
+// @description Rearranges some information on the to-read book list and makes the ASIN copy to clipboard when clicked.
 // ==/UserScript==
 
-// inspired by CH Amazon ASIN Adder by clickhappier https://greasyfork.org/en/scripts/6862-ch-amazon-asin-adder
 
-
-
-$('td.field.title').each(function(){
+$('td.field.title').each(function() {
     var theBookRow = $(this).closest('tr');
     var bookAuthor = theBookRow.children('td.author')[0].innerHTML;
     var bookAsin = theBookRow.children('td.asin')[0].innerHTML;
@@ -29,8 +26,8 @@ $('td.field.title').each(function(){
     $(this).append(asinDiv);
 });
 
-$('div.injectedAsin').on('click',function(){
-        asin = $(this).children('div.value')[0].innerText;
-        GM_setClipboard(asin);
-        $(this).append("\u2713");
-    });
+$('div.injectedAsin').on('click', function() {
+    asin = $(this).children('div.value')[0].innerText;
+    GM_setClipboard(asin);
+    $(this).append("\u2713");
+});
