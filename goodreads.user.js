83f4fd37e62e48a3b3efd6d02346831758dd8bcf
Hezekiah Michael <pianycist@gmail.com>
2022-07-16 20:28:48 -0400

    visual feedback on click, style injected items

--- goodreads.user.js
+++ goodreads.user.js
@@ -4,7 +4,7 @@
 // @match       https://www.goodreads.com/review/list/*
 // @grant       GM_setClipboard
 // @require     https://code.jquery.com/jquery-latest.min.js
-// @version     1.0.0
+// @version     1.0.1
 // @author      holyspiritomb
 // @run-at      document-end
 // @icon        https://www.google.com/s2/favicons?domain=goodreads.com
@@ -13,21 +13,26 @@
 
 
 $('td.field.title').each(function() {
-    var theBookRow = $(this).closest('tr');
-    var bookAuthor = theBookRow.children('td.author')[0].innerHTML;
-    var bookAsin = theBookRow.children('td.asin')[0].innerHTML;
-    var bookAuthorElem = $(`<div></div>`);
-    bookAuthorElem.html(bookAuthor);
-    bookAuthorElem.addClass('injectedAuthor');
-    var asinDiv = $(`<div></div>`);
-    asinDiv.html(bookAsin);
-    asinDiv.addClass('injectedAsin');
-    $(this).append(bookAuthorElem);
-    $(this).append(asinDiv);
+    var bookRow = $(this).closest('tr');
+    var bookAuthor = bookRow.children('td.author')[0].innerHTML;
+    var bookAsin = bookRow.children('td.asin')[0].innerHTML;
+    var authorElem = $(`<div></div>`);
+    authorElem.html(bookAuthor);
+    authorElem.css('margin-top','10px');
+    authorElem.addClass('injectedAuthor');
+    var asinElem = $(`<div></div>`);
+    asinElem.html(bookAsin);
+    asinElem.css('margin-top','10px');
+    asinElem.addClass('injectedAsin');
+    $(this).append(authorElem);
+    $(this).append(asinElem);
 });
 
 $('div.injectedAsin').on('click', function() {
     asin = $(this).children('div.value')[0].innerText;
+    var copiedSpan = $(`<span></span>`);
+    copiedSpan.text('\u2713');
+    copiedSpan.css('color','limegreen');
     GM_setClipboard(asin);
-    $(this).append("\u2713");
+    $(this).append(copiedSpan);
 });
