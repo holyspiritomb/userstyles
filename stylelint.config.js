cc93288c9bc9e3902363c97f2fb6ef146cb3b647
Hezekiah Michael <spiritomb@protonmail.com>
2025-01-15 17:30:21 -0500

    add stylelint stuff

---
+++ stylelint.config.js
@@ -0,0 +1,30 @@
+/** @type {import('stylelint').Config} */
+
+export default {
+  extends: ["stylelint-config-standard"],
+  plugins: ["stylelint-less", "stylelint-use-nesting"],
+  rules: {
+    "at-rule-no-unknown": null,
+    "color-no-invalid-hex":true,
+    "less/color-no-invalid-hex":true,
+    "csstools/use-nesting": "always",
+    "at-rule-empty-line-before":null,
+    "color-function-notation":null,
+    "color-hex-length": null,
+    "custom-property-no-missing-var-function": true,
+    "declaration-no-important":null,
+    "function-calc-no-unspaced-operator":true,
+    "function-linear-gradient-no-nonstandard-direction": null,
+    "function-url-quotes": "always",
+    "length-zero-no-unit":null,
+    "no-descending-specificity": null,
+    "no-duplicate-selectors": true,
+    "no-invalid-position-at-import-rule": null,
+    "rule-empty-line-before": null,
+    "selector-attribute-quotes": "always",
+    "selector-class-pattern":null,
+    "selector-id-pattern":null,
+    "selector-no-qualifying-type": false,
+    "value-no-vendor-prefix":null
+  }
+};
