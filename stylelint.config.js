685173a72716d371fcaae1622fb5b1109df555ec
Hezekiah Michael <spiritomb@protonmail.com>
2025-01-16 15:50:09 -0500

    more lint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,37 +1,49 @@
 /** @type {import('stylelint').Config} */
 import defineConfig from 'stylelint-define-config';
+/// <reference types="@stylelint-types/stylelint-stylus">
+/// <reference types="@stylelint-types/stylelint-order">
+
 export default defineConfig ({
-  extends: ["stylelint-config-standard",],
+  extends: [
+    "stylelint-config-standard",
+    // "stylelint-stylus/standard",
+    // "stylelint-config-standard-less"
+  ],
   plugins: [
-    "stylelint-less",
+    // "stylelint-less",
     "stylelint-use-nesting",
     "stylelint-declaration-block-no-ignored-properties",
-    'stylelint-order'
+    'stylelint-order',
+    'stylelint-stylus'
   ],
   rules: {
     // "at-rule-disallowed-list": ["import"],
-    "at-rule-no-unknown": null,
-    "color-no-invalid-hex":true,
-    "less/color-no-invalid-hex":true,
-    "csstools/use-nesting": "always",
     "at-rule-empty-line-before":null,
+    "at-rule-no-unknown": null,
     "color-function-notation":null,
     "color-hex-length": null,
+    "color-no-invalid-hex":true,
+    "csstools/use-nesting": "always",
+    "comment-empty-line-before": null,
     "custom-property-no-missing-var-function": true,
     "declaration-no-important":null,
     "function-calc-no-unspaced-operator":true,
     "function-linear-gradient-no-nonstandard-direction": null,
     "function-url-quotes": "always",
     "length-zero-no-unit":null,
+    // "less/color-no-invalid-hex":true,
     "no-descending-specificity": null,
     "no-duplicate-selectors": true,
     "no-invalid-position-at-import-rule": null,
+    "plugin/declaration-block-no-ignored-properties": true,
+    "property-no-vendor-prefix": null,
     "rule-empty-line-before": null,
     "selector-attribute-quotes": "always",
     "selector-class-pattern":null,
     "selector-id-pattern":null,
-    "selector-no-qualifying-type": false,
+    "selector-no-qualifying-type": null,
+    "stylus/indentation": 4,
+    "value-keyword-case": null,
     "value-no-vendor-prefix":null,
-    "plugin/declaration-block-no-ignored-properties": true,
   }
 });
