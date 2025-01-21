850db18194e1370d5e6e836f14b2f3eb3f2958b6
Hezekiah Michael <spiritomb@protonmail.com>
2025-01-20 21:06:18 -0500

    add stylelint stuff

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,38 +2,32 @@
 import defineConfig from 'stylelint-define-config';
 /// <reference types="@stylelint-types/stylelint-stylus">
 /// <reference types="@stylelint-types/stylelint-order">
+/// <reference types="@types/postcss-less">
 
 export default defineConfig ({
   extends: [
     "stylelint-config-standard",
-    // "stylelint-stylus/standard",
-    // "stylelint-config-standard-less"
   ],
   plugins: [
-    // "stylelint-less",
-    "@csstools/stylelint-at-risk",
     "stylelint-use-nesting",
     "stylelint-declaration-block-no-ignored-properties",
     'stylelint-order',
-    'stylelint-stylus'
+    // 'stylelint-stylus'
   ],
   rules: {
-    // "at-rule-disallowed-list": ["import"],
-    "@csstools/stylelint-at-risk": true,
     "at-rule-empty-line-before":null,
-    "at-rule-no-unknown": null,
+    "at-rule-no-vendor-prefix": null,
     "color-function-notation":null,
     "color-hex-length": null,
     "color-no-invalid-hex":true,
-    "csstools/use-nesting": "always",
     "comment-empty-line-before": null,
+    "csstools/use-nesting": "always",
     "custom-property-no-missing-var-function": true,
     "declaration-no-important":null,
     "function-calc-no-unspaced-operator":true,
     "function-linear-gradient-no-nonstandard-direction": null,
     "function-url-quotes": "always",
     "length-zero-no-unit":null,
-    // "less/color-no-invalid-hex":true,
     "no-descending-specificity": null,
     "no-duplicate-selectors": true,
     "no-invalid-position-at-import-rule": null,
@@ -44,8 +38,22 @@ export default defineConfig ({
     "selector-class-pattern":null,
     "selector-id-pattern":null,
     "selector-no-qualifying-type": null,
-    "stylus/indentation": 4,
     "value-keyword-case": null,
     "value-no-vendor-prefix":null,
-  }
+    // "stylus/indentation": 4,
+  },
+  overrides: [
+    {
+      files: "skeetdeck/mobile.user.less",
+      customSyntax: "postcss-less",
+      ignoreDisables: true,
+      extends: [ "stylelint-config-standard-less"],
+      plugins: ["stylelint-less",],
+      rules: {
+        "at-rule-no-unknown": null,
+        "less/color-no-invalid-hex": true,
+        "less/no-duplicate-variables": null,
+      }
+    }
+  ]
 });
