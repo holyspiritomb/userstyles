152bf92565adb291fe4c2008c3bbb600e162c76d
Hezekiah Michael <spiritomb@protonmail.com>
2025-03-01 23:43:31 -0500

    use stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,8 @@
-/** @type {import('stylelint').Config} */
+// @ts-check
 import defineConfig from 'stylelint-define-config';
+/// <reference types="@stylelint-types/stylelint-stylistic">
+/// <reference types="@stylelint-types/stylelint-stylus">
+/// <reference types="@stylelint-types/stylelint-order">
 
 export default defineConfig ({
   extends: [
@@ -9,25 +12,29 @@ export default defineConfig ({
     "stylelint-use-nesting",
     "stylelint-declaration-block-no-ignored-properties",
     'stylelint-order',
+    "@stylistic/stylelint-plugin"
   ],
   rules: {
     "alpha-value-notation": null,
     "at-rule-empty-line-before":null,
     "at-rule-no-vendor-prefix": null,
+    "block-no-empty": null,
     "color-function-notation":null,
     "color-hex-length": null,
     "color-no-invalid-hex":true,
     "comment-empty-line-before": null,
-    "csstools/use-nesting": "always",
+    "custom-property-pattern": null,
+    // "csstools/use-nesting": "always",
     "custom-property-empty-line-before": null,
     "custom-property-no-missing-var-function": true,
     "declaration-no-important":null,
     "function-calc-no-unspaced-operator":true,
     "function-linear-gradient-no-nonstandard-direction": null,
     "function-url-quotes": "always",
+    "import-notation": null,
     "length-zero-no-unit":null,
     "no-descending-specificity": null,
-    "no-duplicate-selectors": true,
+    "no-duplicate-selectors": null,
     "no-invalid-position-at-import-rule": null,
     "plugin/declaration-block-no-ignored-properties": true,
     "property-no-vendor-prefix": null,
@@ -39,8 +46,19 @@ export default defineConfig ({
     "selector-no-vendor-prefix": null,
     "value-keyword-case": null,
     "value-no-vendor-prefix":null,
+    "@stylistic/string-quotes": null,
+    "@stylistic/max-line-length": null,
   },
   overrides: [
+    {
+      files: "**/*.user.css",
+      extends: [
+        "@stylistic/stylelint-config",
+      ],
+      rules: {
+        "@stylistic/indentation": 4,
+      }
+    },
     {
       files: "**/*.less",
       customSyntax: "postcss-less",
